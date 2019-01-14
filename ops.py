bdb4c74b1f5c48ff25b6d82a9fde44d7c9bff210
Alex Sherman <asherman1024@gmail.com>
2019-01-14 07:04:07 +0000

    Support build commands with steps

--- ops.py
+++ ops.py
@@ -30,13 +30,38 @@ def str_replace(string, comp = None):
                 prefixes.append(comp + ".")
         value = var_lookup(var, env_vars, prefixes)
         if not value:
-            print("Failed to find variable {}".format(var))
+            print("Failed to find variable {} {}".format(comp, var))
         string = string[:match.span()[0]] + value + string[match.span()[1]:]
     return string
+
+def get_cmds(command):
+    def parse_cmd(cmd, comp = None):
+        if 'cmd' in cmd and type(cmd['cmd']) is str:
+            yield {'comp': comp, 'path': paths[comp], 'cmd': str_replace(cmd['cmd'], comp)}
+        if 'steps' in cmd and type(cmd['steps']) is list:
+            for step in cmd['steps']:
+                yield from get_cmds(step)
+    comps = all_commands[command]
+    return [part for comp, cmds in comps.items() for cmd in cmds for part in parse_cmd(cmd, comp)]
+
 
 paths = {}
 all_commands = defaultdict(lambda: defaultdict(list))
 variables = defaultdict(dict)
+
+#print(json.dumps(list(paths.keys()), indent = 2))
+
+parser = argparse.ArgumentParser(description='Ops commands')
+parser.add_argument('command', nargs="?", help='The command to run')
+parser.add_argument('--vars', action="store_true", help='Print the variables which will be used')
+parser.add_argument('--env', help='Specify an environment (defaults to \'default\')')
+parser.add_argument('--cmds', action="store_true", help='Print the commands that will be run')
+parser.add_argument('--dir', '-d', help='The directory to search in')
+args = parser.parse_args()
+
+if args.dir:
+    os.chdir(args.dir)
+
 for filename in glob.iglob('**/ops.yaml', recursive=True):
     with open(filename) as f:
         comp = yaml.load(f)
@@ -58,15 +83,8 @@ for filename in glob.iglob('**/ops.yaml', recursive=True):
                         varName = var
                     variables[env][varName] = env_vars[var]
 
-#print(json.dumps(list(paths.keys()), indent = 2))
-
-parser = argparse.ArgumentParser(description='Ops commands')
-parser.add_argument('command', nargs="?", choices=list(all_commands.keys()), help='The command to run')
-parser.add_argument('--vars', action="store_true", help='Print the variables which will be used')
-parser.add_argument('--env', help='Specify an environment (defaults to \'default\')')
-parser.add_argument('--cmds', action="store_true", help='Print the commands that will be run')
-args = parser.parse_args()
 env_vars = variables["default"]
+
 if args.env:
     for var in variables[args.env]:
         env_vars[var] = variables[args.env][var]
@@ -75,9 +93,9 @@ if args.vars:
     print(json.dumps(env_vars, indent = 2))
     exit(0)
 
+if args.cmds:
+    print(json.dumps(list(all_commands.keys()), indent = 2))
+    exit(0)
+
 if args.command:
-    comps = all_commands[args.command]
-    for comp, cmds in comps.items():
-        print("Component: {}".format(comp))
-        run_cmds = [str_replace(cmd["cmd"], comp) for cmd in cmds]
-        print(json.dumps(run_cmds, indent = 2))
+    print(json.dumps(get_cmds(args.command), indent = 2))
