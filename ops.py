6227f3669008d2f165851b3d1f776d92a85003b8
Alex Sherman <asherman1024@gmail.com>
2019-01-16 02:02:18 +0000

    Something something improvements

--- ops.py
+++ ops.py
@@ -5,6 +5,7 @@ import json
 from collections import defaultdict
 import argparse
 import re
+import subprocess
 
 def var_lookup(name, env_vars, prefixes = None):
     if prefixes:
@@ -31,23 +32,33 @@ def str_replace(string, comp = None):
         value = var_lookup(var, env_vars, prefixes)
         if not value:
             print("Failed to find variable {} {}".format(comp, var))
-        string = string[:match.span()[0]] + value + string[match.span()[1]:]
+        string = string[:match.span()[0]] + str(value) + string[match.span()[1]:]
     return string
 
 def get_cmds(command):
+    comp = None
+    if '.' in command:
+        comp, command = command.split('.')
     def parse_cmd(cmd, comp = None):
         if 'cmd' in cmd and type(cmd['cmd']) is str:
-            yield {'comp': comp, 'path': paths[comp], 'cmd': str_replace(cmd['cmd'], comp)}
+            output = {'comp': comp, 'cmd': str_replace(cmd['cmd'], comp)}
+            if comp:
+                output['path'] = paths[comp]
+            yield output
         if 'steps' in cmd and type(cmd['steps']) is list:
             for step in cmd['steps']:
                 yield from get_cmds(step)
-    comps = all_commands[command]
-    return [part for comp, cmds in comps.items() for cmd in cmds for part in parse_cmd(cmd, comp)]
 
+    comps = all_commands[command]
+    if comp:
+        return [part for cmd in comps[comp] for part in parse_cmd(cmd, comp)]
+    else:
+        return [part for comp, cmds in comps.items() for cmd in cmds for part in parse_cmd(cmd, comp)]
 
 paths = {}
 all_commands = defaultdict(lambda: defaultdict(list))
 variables = defaultdict(dict)
+files = []
 
 #print(json.dumps(list(paths.keys()), indent = 2))
 
@@ -56,11 +67,17 @@ parser.add_argument('command', nargs="?", help='The command to run')
 parser.add_argument('--vars', action="store_true", help='Print the variables which will be used')
 parser.add_argument('--env', help='Specify an environment (defaults to \'default\')')
 parser.add_argument('--cmds', action="store_true", help='Print the commands that will be run')
-parser.add_argument('--dir', '-d', help='The directory to search in')
+parser.add_argument('--dir', '-d', help='The directory to operate in')
+parser.add_argument('--files', action="store_true", help='List files that will be templated')
 args = parser.parse_args()
 
 if args.dir:
     os.chdir(args.dir)
+
+owd = os.getcwd()
+print(owd)
+
+files = list([filename for filename in glob.iglob('**/*.ops', recursive=True)])
 
 for filename in glob.iglob('**/ops.yaml', recursive=True):
     with open(filename) as f:
@@ -97,5 +114,21 @@ if args.cmds:
     print(json.dumps(list(all_commands.keys()), indent = 2))
     exit(0)
 
+if args.files:
+    print(json.dumps(files, indent = 2))
+    exit(0)
+
+for filename in files:
+    with open(filename[:-4], 'w') as newf:
+        with open(filename) as f:
+            for line in f.readlines():
+                newf.write(str_replace(line, None))
+
 if args.command:
-    print(json.dumps(get_cmds(args.command), indent = 2))
+    for cmd in get_cmds(args.command):
+        os.chdir(owd)
+        if 'path' in cmd:
+            os.chdir(cmd['path'])
+        print(cmd["cmd"])
+        ret = subprocess.call(cmd["cmd"], shell=True)
+    # print(json.dumps(get_cmds(args.command), indent = 2))
